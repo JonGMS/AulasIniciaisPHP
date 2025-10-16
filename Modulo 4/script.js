529568fcb34d44a763ea47be3bd76735b46cc8fd
JonGMS <joaoggabriell@gmail.com>
2025-10-16 20:38:24 -0300

    Modulo 4 - JavaScript PRINCIPIOS

---
+++ Modulo 4/script.js
@@ -0,0 +1,20 @@
+
+console.log("Hello World!! - Ficheiro Externo");
+//Comentario
+var nome= "João Gabriel"; // String
+var idade = 21; // int
+var administrador = true; //boolean
+
+var cidade = "Lisboa";
+
+var valor1 = 100, nome2= "Maria Eduarda", apelido= "Duda"
+
+var descVar= ""
+let desc = "let é somente acessivel dentro das suas '{}' onde ele foi declarado."
+
+const desc1 = "const é constante e imutavel, quando declarado não pode ser alterado e também só sera acessivel dentro das suas '{}' onde foi declarado";
+
+console.log(desc)
+console.log(desc1)
+
+alert(desc1);
