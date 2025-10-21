2d1d3d73a0b92a9592566770d71caa4cd8f6dcc1
JonGMS <joaoggabriell@gmail.com>
2025-10-20 22:56:12 -0300

    Aula 4 - GetElement parte 1/4

---
+++ Modulo 4/Aula 4/script.js
@@ -0,0 +1,12 @@
+//botao executar
+function button_executar(){
+    document.getElementById('noticia')
+    console.log(document)
+
+    let e = document.getElementById('titulo')
+    console.log(e.id)
+    console.log(e.className)
+    console.log(e.innerHTML)
+    console.log(e.innerText)
+    console.log(e.textContent)
+}
