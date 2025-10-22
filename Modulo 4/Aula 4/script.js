4c30028729e7c122112100127fa647c45fb896fd
JonGMS <joaoggabriell@gmail.com>
2025-10-21 23:34:32 -0300

    Event Handlers

--- Modulo 4/Aula 4/script.js
+++ Modulo 4/Aula 4/script.js
@@ -4,25 +4,6 @@ const estilos = window.getComputedStyle(cor)
 cor.style.backgroundColor = "blue"
 console.log(cor.style.backgroundColor)
 console.log(estilos.getPropertyValue('color'))
-//botao executar
-function button_executar(){
-    document.getElementById('noticia')
-    console.log(document)
-
-    let e = document.getElementById('titulo')
-    console.log(e.id)
-    console.log(e.className)
-    console.log(e.innerHTML)
-    console.log(e.innerText)
-    console.log(e.textContent)
-
-    let d = documento.querySelector('#titulo')
-    console.log(d)
-
-    let a = documenta.querySelectorAll('p')
-
-}
-
 let el = null
 /* Busca o primeiro Elemento do documento */
 el = document.querySelector('*')
@@ -36,4 +17,24 @@ el = document.querySelector('p')
 
 //busca todos os paragrafos de um documento
 el = document.querySelectorAll('p')
+//botao executar
+function button_executar(){
+    document.getElementById('noticia')
+    console.log(document)
+
+    let e = document.getElementById('titulo-noticia')
+    console.log(e.id)
+    console.log(e.className)
+    console.log(e.innerHTML)
+    console.log(e.innerText)
+    console.log(e.textContent)
+
+    let d = document.querySelector('#titulo')
+    console.log(d)
+
+    let a = document.querySelectorAll('p')
+
+}
+
+
 
