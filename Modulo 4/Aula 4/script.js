d66fea0db8d8c28b3c981f9e2e60cc1cb3893fbe
JonGMS <joaoggabriell@gmail.com>
2025-10-21 23:09:13 -0300

    Query Selector

--- Modulo 4/Aula 4/script.js
+++ Modulo 4/Aula 4/script.js
@@ -1,3 +1,9 @@
+let cor = document.querySelector("#noticia");
+
+const estilos = window.getComputedStyle(cor)
+cor.style.backgroundColor = "blue"
+console.log(cor.style.backgroundColor)
+console.log(estilos.getPropertyValue('color'))
 //botao executar
 function button_executar(){
     document.getElementById('noticia')
@@ -9,4 +15,25 @@ function button_executar(){
     console.log(e.innerHTML)
     console.log(e.innerText)
     console.log(e.textContent)
+
+    let d = documento.querySelector('#titulo')
+    console.log(d)
+
+    let a = documenta.querySelectorAll('p')
+
 }
+
+let el = null
+/* Busca o primeiro Elemento do documento */
+el = document.querySelector('*')
+
+// busca todos os elementos do documento
+el = document.querySelectorAll('*')
+
+// Busca o primeiro paragrago de um documento
+
+el = document.querySelector('p')
+
+//busca todos os paragrafos de um documento
+el = document.querySelectorAll('p')
+
