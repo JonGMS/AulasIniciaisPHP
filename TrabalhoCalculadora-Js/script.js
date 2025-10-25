3d13d00651479bf96f44bdd5fd95e4235803dea8
JonGMS <joaoggabriell@gmail.com>
2025-10-25 14:27:37 -0300

    Calculadora: Validação de divisão por 0 (sem bloqueio do teclado). Botão resultado (sem bloqueio de teclado ou adição de histórico)

--- TrabalhoCalculadora-Js/script.js
+++ TrabalhoCalculadora-Js/script.js
@@ -1,7 +1,6 @@
 //Fazer o calculo com base no div - calculo
 var algorismos = []
-
-
+var armazenarHistorico = ''
 var operador
 var resultado
 
@@ -11,26 +10,36 @@ function ObterNumero(operador) {
     try {
         var numero = document.getElementById("numeros");
         var calculo = document.getElementById("calculo")
+        ReiniciarCalculo(operador, calculo, numero);
 
         if (calculo.textContent != '' && numero.value == '') {
             calculo.textContent = calculo.textContent.replace(/[^0-9,]/g, '') + operador;
             throw new Error(mensagemErro = "Altera o operador para: " + operador);
         }
-        else if(numero.value == ''){
-            throw new Error( mensagemErro = "Nenhum número para alterar o Operador");
+        else if (numero.value == '') {
+            throw new Error(mensagemErro = "Nenhum número para alterar o Operador");
         }
 
         var numeroValue = parseFloat(numero.value)
         numeroCalculo = parseFloat(calculo.textContent)
-        EfetuarCalculo(operador, numeroValue)
+        EfetuarCalculo(operador, numeroValue, numero)
     }
     catch {
         console.log(mensagemErro)
     }
 }
 
+function ReiniciarCalculo(operador, calculo, numero) {
+    botaoResultado = document.getElementById('resultado');
+    if (botaoResultado.textContent == 'A/C' && operador == '=') {
+        botaoResultado.textContent = '=';
+        calculo.textContent = '';
+        numero.value = '';
+        // 
+    }
+}
 
-function EfetuarCalculo(operador, numeroUsuario) {
+function EfetuarCalculo(operador, numeroUsuario, numero) {
     try {
         if (operador == "+") {
 
@@ -55,26 +64,51 @@ function EfetuarCalculo(operador, numeroUsuario) {
             ApagarNumeros()
         }
         else if (operador == "/") {
+
             resultado = CalculoPorOperador()
 
             calculo.textContent = resultado + operador
 
             ApagarNumeros()
         }
+        else if (operador == "=") {
+            resultado = CalculoPorOperador()
+
+            console.log(resultado)
+
+            numero.value = resultado
+
+            calculo.textContent = "Resultado"
+
+            botaoResultado = document.getElementById('resultado')
+
+            botaoResultado.textContent = 'A/C'
+
+            //Bloquear Teclado
+            //Adicionar todo o calculo no histórico
+        }
+        DivisaoZero();
     }
     catch {
-        console.log("Erro no EfetuarCalculo")
+        console.log('Erro em Efetuar Calculo')
     }
 
-
+    function DivisaoZero() {
+        if (resultado == "Infinity") {
+            calculo.textContent = "Não é possível dividir por zero";
+            //Bloquear Cliques em botões
+            // Atualizar Botão de =
+        }
+    }
 
     function CalculoPorOperador() {
 
         if (calculo.textContent.includes("+")) {
+            //    armazenarHistorico = armazenarHistorico + numero.value + operador
             return resultado = numeroUsuario + numeroCalculo
         }
         else if (calculo.textContent.includes("-")) {
-            return resultado = numeroUsuario - numeroCalculo
+            return resultado = numeroCalculo - numeroUsuario
         }
         else if (calculo.textContent.includes("*")) {
             return resultado = numeroUsuario * numeroCalculo
@@ -86,13 +120,17 @@ function EfetuarCalculo(operador, numeroUsuario) {
             console.log("Primeiro Calculo - Nenhuma formula aplicada.")
             return numeroUsuario
         }
+
     }
 }
-
 
 function EventoTeclado(digito) {
     let numeros = document.getElementById('numeros')
     numeros.value = numeros.value + digito
+}
+function ApagarNumeros() {
+    let numeros = document.getElementById('numeros')
+    numeros.value = ''
 }
 
 function AlterarTexto(e) {
@@ -103,9 +141,4 @@ function AlterarTexto(e) {
     } else {
         console.warn("Elemento <h1> não encontrado.");
     }
-
-}
-function ApagarNumeros() {
-    let numeros = document.getElementById('numeros')
-    numeros.value = ''
 }
