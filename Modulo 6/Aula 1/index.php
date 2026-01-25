9867aa0bc033dd0f7fcbfece8e087f6593b17710
JonGMS <joaoggabriell@gmail.com>
2026-01-24 22:19:25 -0300

    Apresentação ao PHP continuação - PHP dentro do HTML

--- Modulo 6/Aula 1/index.php
+++ Modulo 6/Aula 1/index.php
@@ -30,7 +30,7 @@
         <div class="atividades">
             <div class="painel-tag">
                 <h3 class="titulo-aula">Tag de abertura</h3>
-                
+
                 <h3 class="titulo-aula">Variaveis</h3>
             </div>
 
@@ -66,23 +66,23 @@
                 $valor_str = "145";
                 $valor_int = (int)$valor_str; // Console.Convert.Toint64(valor_str)
 
-                if(is_int($valor_int)){
+                if (is_int($valor_int)) {
                     echo "A conversão funcionou: $valor_int <br>";
                 }
-                
+
                 $valor_null = null;
                 echo "Valor: $valor_null <br>";
                 var_dump($valor_null);
                 //function Mensagem(mixed $value) {};
-                if(is_null($valor_null)){
+                if (is_null($valor_null)) {
                     unset($valor_null); //Exclui uma variavel
                     echo "<br> Variavel excluida <br>";
                 }
                 //Agora a variavel não tem valor, com empty conseguimos retornar um bool quando verificamos uma variavel vazia ou nula
-                if(empty($valor_null)){
-                    echo "A variavel esta vazia ou nula. <br>" ;
+                if (empty($valor_null)) {
+                    echo "A variavel esta vazia ou nula. <br>";
                 }
-                
+
                 $nome = "Joao";
                 $sobrenome = "Santos";
 
@@ -90,10 +90,10 @@
                 echo $nome_completo,  "<br>";
 
                 $j = 0;
-                
-                for ($i = 0; $i < strlen($nome_completo); $i++){
+
+                for ($i = 0; $i < strlen($nome_completo); $i++) {
                     $j--;
-                    echo $nome_completo[$i], " ", $nome_completo[$j] ,"<br>";
+                    echo $nome_completo[$i], " ", $nome_completo[$j], "<br>";
                 }
 
                 $personalizado = strtoupper($nome_completo);
@@ -105,28 +105,27 @@
 
                 echo strlen($personalizado), "<br>";
 
-                if(str_contains($personalizado, "LINDOO")){
+                if (str_contains($personalizado, "LINDOO")) {
                     echo "Contem JOAO";
-                }
-                else{
-                    echo "Não Contem"; 
+                } else {
+                    echo "Não Contem";
                 }
 
                 $caixa = array("Caneta", "Celular", "Monjaro", "Durateston", "Remédio", "Boneco", 10 => "Lego"); //O for vai parar no $i = 7, então nunca vai chegar no 10 = lego
-                for($i = 0; $i < count($caixa); $i++){
-                    if(empty($caixa[$i])){
+                for ($i = 0; $i < count($caixa); $i++) {
+                    if (empty($caixa[$i])) {
 
                         continue;
                     }
-                    echo "<br>", $i, " " ,$caixa[$i];
+                    echo "<br>", $i, " ", $caixa[$i];
                 }
-                foreach($caixa as $i => $conteudoCaixa){
-                    echo "<br>", $i, " " , $conteudoCaixa;
+                foreach ($caixa as $i => $conteudoCaixa) {
+                    echo "<br>", $i, " ", $conteudoCaixa;
 
-                    if(str_contains($caixa[$i], "Lego")){
-                        unset($caixa[$i]); 
+                    if (str_contains($caixa[$i], "Lego")) {
+                        unset($caixa[$i]);
                     }
-                } 
+                }
 
                 $empresa = array(
                     "Administrador" => "Maria Eduarda",
@@ -134,29 +133,49 @@
                     "Designer" => "Thiago",
                     "Desenvolvedor" => "João"
                 );
-                
+
                 echo "<br>", '<pre>';
 
                 print_r($empresa);
                 echo '</pre>';
-                
-                $empresa_multidimensional = [
-                    'Administrador' =>[
-                    '1' => ['nome' => 'Maria E.', 'telefone' => '123456789', 'email' => 'mariaadm@gmail.com'],
-                    '2' => ['nome' => 'Amabile', 'telefone' => '8521234658', 'email' => 'mabyadm@gmail.com'],
 
-                    'Vendedor' =>['nome' => 'Elian','telefone' => '987654321', 'email' => 'Elian@gmail.com' ],
-                    'Designer' =>['nome' => 'Thiago','telefone' => '741852963', 'email' => 'thi@gmail.com' ],
-                    'Desenvolvedor' =>['nome' => 'Joao','telefone' => '369258147', 'email' => 'jon@gmail.com' ]
-                ]];
+                $empresa_multidimensional = [
+                    'Administrador' => [
+                        '1' => ['nome' => 'Maria E.', 'telefone' => '123456789', 'email' => 'mariaadm@gmail.com'],
+                        '2' => ['nome' => 'Amabile', 'telefone' => '8521234658', 'email' => 'mabyadm@gmail.com'],
+
+                        'Vendedor' => ['nome' => 'Elian', 'telefone' => '987654321', 'email' => 'Elian@gmail.com'],
+                        'Designer' => ['nome' => 'Thiago', 'telefone' => '741852963', 'email' => 'thi@gmail.com'],
+                        'Desenvolvedor' => ['nome' => 'Joao', 'telefone' => '369258147', 'email' => 'jon@gmail.com']
+                    ]
+                ];
                 echo '<pre>';
                 print_r($empresa_multidimensional);
                 echo '</pre>';
 
                 echo $empresa_multidimensional['Administrador'][2]['nome'];
+
+                var_dump(20 <=> 10); //retorna 1
+                var_dump(10 <=> 10); //retorna 0
+                var_dump(10 <=> 20); //retorna -1
+                //São as respectivas posições '1' (Primeiro valor), '0' (simbolo de '=') e '-1' (segundo valor)
+
+                $variavel = "Joao";
+                echo $variavel == "joao" ? "SIM" : "NÃO"
+
                 ?>
-                
+
             </div>
+            <?php $valor = 100; ?>
+
+
+            <?php if ($valor == 100) : ?>
+                <p> Valor é 100.</p>
+            <?php elseif ($valor < 100) : ?>
+                <p> Valor é menor que 100.</p>
+            <?php else : ?>
+                <p> Valor é diferente de todas as outras condições.</p>
+            <?php endif; ?>
 
         </div>
     </div>
