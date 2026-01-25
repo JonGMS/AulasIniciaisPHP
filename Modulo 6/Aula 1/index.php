e81d9ad81de37a37aac0879e0097af9e3c8a4ad5
JonGMS <joaoggabriell@gmail.com>
2026-01-25 14:54:17 -0300

    Apresentação ao PHP: Utilizando parte do conteudo em um pequeno projeto. - Simulação de notas escolares

--- Modulo 6/Aula 1/index.php
+++ Modulo 6/Aula 1/index.php
@@ -2,12 +2,69 @@
 <html lang="en">
 
 <head>
+    <link rel="stylesheet" href="../../assets/style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="../../assets/style.css">
+    
+    <style> 
+        .menuSimulacao{
+            display: flex;
+            align-items: center;
+        }
+        .button-simulacao{
+            margin-inline-start: 10px;
+            text-align: center;
+            height: 20px;
+            width: 100px;
+            padding: 10px;
+            background-color: darkslategray;
+            border-radius: 10px;
+            color: white;
+        }
+        .button-simulacao:hover{
+            color: darkblue;
+            background-color: white;
+        }
+        th {
+            background-color: darkslategray;
+            color: white;
+        }
+
+        .notaReprovada {
+            color: red;
+        }
+
+        .statusReprovado {
+            background-color: rgb(251, 134, 134);
+            color: rgb(137, 32, 32);
+        }
+
+        .notaAprovado {
+            color: darkblue;
+        }
+
+        .statusAprovado {
+            background-color: rgb(68, 224, 81);
+            color: green;
+        }
+
+        th,
+        td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: center;
+        }
+    </style>
     <title>PHP - Variaveis</title>
 </head>
+<?php
+include_once "functions.php";
+include_once "dados.php";
+$alunos = DarNotas($alunos);
+$alunos = FazerMedias($alunos);
+
+?>
 
 <body>
     <header class="header-tela">
@@ -28,155 +85,29 @@
     </header>
     <div class="content-wrapper">
         <div class="atividades">
-            <div class="painel-tag">
-                <h3 class="titulo-aula">Tag de abertura</h3>
+            
+            <h3 class="titulo-aula">Atividade Simulada</h3>
 
-                <h3 class="titulo-aula">Variaveis</h3>
+
+            <div class="menuSimulacao">
+                <h3>Informática</h3>
+                <div class="button-simulacao">CLASSE</div>
+                <div class="button-simulacao">CARGOS</div>
             </div>
-
-            <div class="script-php">
+            <table style="width:100%; border: solid 1px black;border-collapse: collapse;">
+                <tr style=>
+                    <th>NOME</th>
+                    <th>1º SEMESTRE</th>
+                    <th>2º SEMESTRE</th>
+                    <th>3º SEMESTRE</th>
+                    <th>4º SEMESTRE</th>
+                    <th>MÉDIA</th>
+                    <th>STATUS</th>
+                </tr>
                 <?php
-
-                $numero_inteiro = 10; //Int
-
-                $numero_decimal = 0.5; // float
-
-                $texto = "Esse é o resultado: "; //string
-
-                $verdadeiro_falso = true; //bool
-
-                const TAXA_VARIAVEL = 1.2; //constante menos comum
-
-                define('TAXA_FIXA', 10); // constante comum
-
-                if ($verdadeiro_falso == true) { //Declaração de if
-                    $resultado = $numero_inteiro + $numero_decimal * TAXA_VARIAVEL + TAXA_FIXA;
-                    echo "<h1>$texto $resultado</h1>" . '<br>'; //Comparavel com console.WriteLine com DOM do JS
-                } else {
-                    $resultado = $numero_inteiro + $numero_decimal + TAXA_FIXA;
-                    echo "<h1>$texto $resultado</h1>" . '<br>'; //Comparavel com console.WriteLine com DOM do JS
-                }
-
-                echo 'Versão do PHP: ', PHP_VERSION, "<br>";
-
-                echo 'Linha do código: ', __LINE__, "<br>";
-
-                // 
-
-                $valor_str = "145";
-                $valor_int = (int)$valor_str; // Console.Convert.Toint64(valor_str)
-
-                if (is_int($valor_int)) {
-                    echo "A conversão funcionou: $valor_int <br>";
-                }
-
-                $valor_null = null;
-                echo "Valor: $valor_null <br>";
-                var_dump($valor_null);
-                //function Mensagem(mixed $value) {};
-                if (is_null($valor_null)) {
-                    unset($valor_null); //Exclui uma variavel
-                    echo "<br> Variavel excluida <br>";
-                }
-                //Agora a variavel não tem valor, com empty conseguimos retornar um bool quando verificamos uma variavel vazia ou nula
-                if (empty($valor_null)) {
-                    echo "A variavel esta vazia ou nula. <br>";
-                }
-
-                $nome = "Joao";
-                $sobrenome = "Santos";
-
-                $nome_completo = $nome . " " . $sobrenome;
-                echo $nome_completo,  "<br>";
-
-                $j = 0;
-
-                for ($i = 0; $i < strlen($nome_completo); $i++) {
-                    $j--;
-                    echo $nome_completo[$i], " ", $nome_completo[$j], "<br>";
-                }
-
-                $personalizado = strtoupper($nome_completo);
-
-                echo $personalizado, "<br>";
-
-                $personalizado = substr($personalizado, 0, 6);
-                echo $personalizado, "<br>";
-
-                echo strlen($personalizado), "<br>";
-
-                if (str_contains($personalizado, "LINDOO")) {
-                    echo "Contem JOAO";
-                } else {
-                    echo "Não Contem";
-                }
-
-                $caixa = array("Caneta", "Celular", "Monjaro", "Durateston", "Remédio", "Boneco", 10 => "Lego"); //O for vai parar no $i = 7, então nunca vai chegar no 10 = lego
-                for ($i = 0; $i < count($caixa); $i++) {
-                    if (empty($caixa[$i])) {
-
-                        continue;
-                    }
-                    echo "<br>", $i, " ", $caixa[$i];
-                }
-                foreach ($caixa as $i => $conteudoCaixa) {
-                    echo "<br>", $i, " ", $conteudoCaixa;
-
-                    if (str_contains($caixa[$i], "Lego")) {
-                        unset($caixa[$i]);
-                    }
-                }
-
-                $empresa = array(
-                    "Administrador" => "Maria Eduarda",
-                    "Vendedor" => "Elian",
-                    "Designer" => "Thiago",
-                    "Desenvolvedor" => "João"
-                );
-
-                echo "<br>", '<pre>';
-
-                print_r($empresa);
-                echo '</pre>';
-
-                $empresa_multidimensional = [
-                    'Administrador' => [
-                        '1' => ['nome' => 'Maria E.', 'telefone' => '123456789', 'email' => 'mariaadm@gmail.com'],
-                        '2' => ['nome' => 'Amabile', 'telefone' => '8521234658', 'email' => 'mabyadm@gmail.com'],
-
-                        'Vendedor' => ['nome' => 'Elian', 'telefone' => '987654321', 'email' => 'Elian@gmail.com'],
-                        'Designer' => ['nome' => 'Thiago', 'telefone' => '741852963', 'email' => 'thi@gmail.com'],
-                        'Desenvolvedor' => ['nome' => 'Joao', 'telefone' => '369258147', 'email' => 'jon@gmail.com']
-                    ]
-                ];
-                echo '<pre>';
-                print_r($empresa_multidimensional);
-                echo '</pre>';
-
-                echo $empresa_multidimensional['Administrador'][2]['nome'];
-
-                var_dump(20 <=> 10); //retorna 1
-                var_dump(10 <=> 10); //retorna 0
-                var_dump(10 <=> 20); //retorna -1
-                //São as respectivas posições '1' (Primeiro valor), '0' (simbolo de '=') e '-1' (segundo valor)
-
-                $variavel = "Joao";
-                echo $variavel == "joao" ? "SIM" : "NÃO"
-
+                Boletim($alunos)
                 ?>
-
-            </div>
-            <?php $valor = 100; ?>
-
-
-            <?php if ($valor == 100) : ?>
-                <p> Valor é 100.</p>
-            <?php elseif ($valor < 100) : ?>
-                <p> Valor é menor que 100.</p>
-            <?php else : ?>
-                <p> Valor é diferente de todas as outras condições.</p>
-            <?php endif; ?>
-
+            </table>
         </div>
     </div>
 </body>
