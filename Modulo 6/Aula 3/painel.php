f7e48a20c143c32d913cb04da4781742d3ac6d34
JonGMS <joaoggabriell@gmail.com>
2026-01-30 20:15:14 -0300

    Apresentação ao PHP: Novo projeto - Clube do livro 5%

---
+++ Modulo 6/Aula 3/painel.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style.css">
+    <!-- #4B4B44
+ -->
+    <title></title>
+</head>
+
+<body>
+    <header>
+        <div class="painel-button">
+            <a href="index.php">
+                <div class="buttom-menu"><img src="" alt=""></div>
+            </a>
+        </div>
+
+        <div class="painel-menu">
+            <div class="painel-menu-grid">
+                <div class="titulo">
+                    <h1 class="legenda-tipo">MENU</h1>
+                </div>
+
+                <div class="campo-pesquisa">
+                    <form class="formulario-pesquisa" action="" method="get">
+                        <input type="text" name="" id="pesquisa">
+                    </form>
+                </div>
+                <div class="conta-info">
+                    <p class="nome-usuario">Jon</p>
+                    <img class="imagem-perfil" src="assets/images/MembroOffiline.png" alt="">
+                </div>
+            </div>
+
+        </div>
+
+    </header>
+    <div class="content">
+        <div class="menu-button">
+            <div class="menu-fixed">
+
+                <a href="" class="button1-menu"><div class="button1-menu">
+                    <img src="" alt="">
+                    LIVRO
+                </div></a>
+                
+                <a href="" class="button2-menu"><div class="button2-menu">
+                    <img src="" alt="">
+                    LOCAÇÃO
+                </div></a>
+                
+                <a href="" class="button3-menu"><div class="button3-menu">
+                    <img src="" alt="">
+                    MEMBRO
+                </div></a>
+                
+            </div>
+        </div>
+
+        <div class="content-wraper">
+
+        </div>
+    </div>
+
+</body>
+
+</html>
