fca2ba621fced76392e5edc4653f3366234064cb
JonGMS <joaoggabriell@gmail.com>
2026-02-09 00:19:18 -0300

    Atualização de antigas aulas

--- Modulo 6/Aula 3/index.php
+++ Modulo 6/Aula 3/index.php
@@ -1,15 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+session_start();
+
+if (empty($_SESSION['usuario'])) {
+    header('Location: ../Aula 3/login.php');
+    return;
+}
+?>
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
-    
+    <link rel="stylesheet" href="/assets/style.css">
+
 </head>
 
 <body>
-    <?php require_once "painel.php" ?>
+
+    <div class="content-wraper"> <?php require_once "painel.php" ?>
+
+    </div>
 
 </body>
 
