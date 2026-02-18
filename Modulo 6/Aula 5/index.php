29515ae8c85c88363f5245df53cdee867165347b
JonGMS <joaoggabriell@gmail.com>
2026-02-17 22:22:42 -0300

    Calculadora PHP: Design

---
+++ Modulo 6/Aula 5/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style.css">
+    <title>Calculadora PHP</title>
+</head>
+<body>
+    <div class="painel_calculadora">
+        <div class="painel_campo">
+            <label for="">Calculadora PHP</label><br>
+            <input class="caixa_texto" type="text" name="" id="">
+        </div>
+        <div class="painel_teclado_numerico">
+            <div class="teclado-numerico">
+                <div class="tecla7 tecla teclas-ativadas">7</div>
+                <div class="tecla8 tecla teclas-ativadas">8</div>
+                <div class="tecla9 tecla teclas-ativadas">9</div>
+                <div class="tecla4 tecla teclas-ativadas">4</div>
+                <div class="tecla5 tecla teclas-ativadas">5</div>
+                <div class="tecla6 tecla teclas-ativadas">6</div>
+                <div class="tecla1 tecla teclas-ativadas">1</div>
+                <div class="tecla2 tecla teclas-ativadas">2</div>
+                <div class="tecla3 tecla teclas-ativadas">3</div>
+                <div class="tecla-virgula tecla teclas-ativadas">.</div>
+                <div class="tecla0 tecla teclas-ativadas">0</div>
+                <div class="tecla-soma teclas-ativadas">+</div>
+                <div class="tecla-sub teclas-ativadas">-</div>
+                <div class="teclax teclas-ativadas">x</div>
+                <div class="tecla-divisao teclas-ativadas">÷</div>
+                <div class="tecla-resultado" id="resultado">=</div>
+            </div>
+        </div>
+        <div class="painel_historio">
+            
+        </div>
+        
+    </div>
+</body>
+</html>
