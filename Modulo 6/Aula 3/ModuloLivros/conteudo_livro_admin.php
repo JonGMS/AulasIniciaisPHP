21bf43dac524fde6a8efe9bca582d95d2cf133c5
JonGMS <joaoggabriell@gmail.com>
2026-02-20 23:24:30 -0300

    Orientação a Objetos: Clube do Livro - Cadastro de livros 20%

---
+++ Modulo 6/Aula 3/ModuloLivros/conteudo_livro_admin.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/livro_admin.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+    <div class="cadastrar_livro">
+
+        <form action="" method="post">
+            <h2>CADASTRAR</h2>
+            <div class="campo_nome">
+                <label for="text_nome">Nome</label><br>
+                <input type="text" class="input_text" name="text_nome" id="text_nome">
+            </div>
+            <div class="campo_autor">
+                <label for="text_autor">Autor</label><br>
+                <input type="text" class="input_text" name="text_autor" id="text_autor">
+            </div>
+            <div class="campo_quantidade">
+                <label for="text_quantidade">Quantidade</label><br>
+                <input type="number" class="input_text" name="text_quantidade" id="text_quantidade">
+            </div>
+            <div class="campo_genero">
+                <label>Gêneros:</label><br>
+                <div class="checkbox-group">
+                    
+                        <label><input type="checkbox" name="generos[]" value="Fantasia"> Fantasia</label>
+                        <label><input type="checkbox" name="generos[]" value="Ficcao Cientifica"> Ficção Científica</label>
+                        <label><input type="checkbox" name="generos[]" value="Romance"> Romance</label>
+                        <label><input type="checkbox" name="generos[]" value="Misterio"> Mistério</label>
+                        <label><input type="checkbox" name="generos[]" value="Suspense"> Suspense</label>
+                    
+                        <label><input type="checkbox" name="generos[]" value="Terror"> Terror</label>
+                        <label><input type="checkbox" name="generos[]" value="Drama"> Drama</label>
+                        <label><input type="checkbox" name="generos[]" value="Distopia"> Biografia</label>
+                        <label><input type="checkbox" name="generos[]" value="Distopia"> AutoAjuda</label>
+                        <label><input type="checkbox" name="generos[]" value="Distopia"> Educação</label>
+                    
+
+
+                </div>
+            </div>
+            <div class="campo_colecao">
+                <label for="text_colecao">Coleção</label><br>
+                <input type="text" class="input_text" name="text_colecao" id="text_colecao">
+            </div>
+            <div class="campo_imagem">
+                <label for="capa">Imagem</label><br>
+                <input type="file" name="capa" id="capa">
+            </div>
+
+        </form>
+
+    </div>
+    <div class="listar_livro">
+
+    </div>
+
+</body>
+
+</html>
