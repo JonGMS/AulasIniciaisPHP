5c2a496854d55fe8b0ff056054aafe831c9b0416
JonGMS <joaoggabriell@gmail.com>
2026-03-12 18:27:38 -0300

    Orientação a Objetos: Clube do livro - Atualização no armazenamento de livros para ADMIN

--- Modulo 6/Aula 3/ModuloLivros/valida_adicao.php
+++ Modulo 6/Aula 3/ModuloLivros/valida_adicao.php
@@ -9,38 +9,34 @@ $id = RetornarID();
 if (!isset($_SESSION['id_livro_locacao'])) {
     $_SESSION['id_livro_locacao'] = [];
 }
-
-foreach ($_SESSION['id_livro_locacao'] as  $indice => $id_livro) {
-    if ($_GET['livro'] == $_SESSION['id_livro_locacao'][$indice]) {
-        header('Location: livros.php');
-
-        exit();
-    }
-}
-
-if ($_SESSION['usuario'] == "ADMIN") {
-
-    if (!isset($_SESSION['id_membro_admin'])) {
-        $_SESSION['id_membro_admin'] = [];
-    }
-
-    $idLivro = $_GET['livro'];
-    if (!in_array($idLivro, $_SESSION['id_membro_admin'])) {
-        $_SESSION['id_membro_admin'][] = $idLivro;
-    }
-    header('Location: livros.php');
-
-    exit();
+if (!isset($_SESSION['admin_relacoes'])) {
+    $_SESSION['admin_relacoes'] = [];
 }
 
 if (isset($_GET['livro'])) {
-    $_SESSION['id_livro_locacao'][] = $_GET['livro'];
+    $idLivro = $_GET['livro'];
+
+    if ($_SESSION['usuario'] != "ADMIN") {
+        if (!in_array($idLivro, $_SESSION['id_livro_locacao'])) {
+            $_SESSION['id_livro_locacao'][] = $idLivro;
+        }
+    } else {
+
+        $idMembro = RetornarID();
+
+
+        if (!isset($_SESSION['admin_relacoes'][$idMembro])) {
+            $_SESSION['admin_relacoes'][$idMembro] = [];
+        }
+
+        if (!in_array($idLivro, $_SESSION['admin_relacoes'][$idMembro])) {
+            $_SESSION['admin_relacoes'][$idMembro][] = $idLivro;
+        }
+    }
 }
 
 header('Location: livros.php');
-
 exit();
-
 
 function RetornarID()
 {
